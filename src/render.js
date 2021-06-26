390f149ab3279134469033b105b69b93474fa1a8
jdubbleu <jwhitlow45@gmail.com>
2021-06-26 18:02:31 -0400

    feat:create navigation bar for calendar

--- src/render.js
+++ src/render.js
@@ -1,5 +1,20 @@
-const container = document.getElementById("container");
+// event listener for nav bar arrows
+const left_arrow = document.getElementById("left-arrow");
+const right_arrow = document.getElementById("right-arrow");
 
+left_arrow.addEventListener("click", moveLeft);
+right_arrow.addEventListener("click", moveRight);
+
+function moveLeft() {
+  console.log("left");
+}
+
+function moveRight() {
+  console.log("right");
+}
+
+// draw month view grid
+const container = document.getElementById("container");
 function makeRows(rows, cols) {
   container.style.setProperty("--grid-rows", rows);
   container.style.setProperty("--grid-cols", cols);
@@ -9,5 +24,4 @@ function makeRows(rows, cols) {
     container.appendChild(cell).className = "column";
   }
 }
-
-makeRows(5,7);
+makeRows(5, 7);
