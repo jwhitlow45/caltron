d90064a31da5536e264871066c5d5dfdfd9da8ff
jdubbleu <jwhitlow45@gmail.com>
2021-06-27 01:57:52 -0400

    feat:add function to build weekday headers

--- src/render.js
+++ src/render.js
@@ -1,3 +1,17 @@
+//global values
+const calendar = document.getElementById("calendar"); // calendar div
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+var page = 0; // current page of calendar
+// if month is nov, dev is 1 and oct is -1
 
 // event listener for nav bar arrows
 const left_arrow = document.getElementById("left-arrow");
@@ -14,15 +28,30 @@ function moveRight() {
   console.log("right");
 }
 
-// draw month view grid
-const container = document.getElementById("container");
-function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
-  for (c = 0; c < rows * cols; c++) {
-    let cell = document.createElement("div");
-    cell.innerText = c + 1;
-    container.appendChild(cell).className = "column";
+function makeWeekdayHeader() {
+  const weekday_header = document.getElementById("weekday-header");
+  for (i=0;i<7;i++){
+    let day = document.createElement("div");
+    day.innerText = days[i];
+    day.id = "weekday-text";
+    weekday_header.appendChild(day);
   }
 }
-makeRows(6, 7);
+
+// draw month view grid
+function makeRows(rows, cols) {
+  const calendar_grid = document.getElementById("calendar-grid");
+  calendar_grid.style.setProperty("--grid-rows", rows);
+  calendar_grid.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    calendar_grid.appendChild(cell).className = "column";
+  }
+}
+
+function startup() {
+  makeWeekdayHeader();
+  makeRows(6,7);
+}
+
+startup();
