8c0e363fc82e41d1a188043d3314f5715d63ffb9
jdubbleu <jwhitlow45@gmail.com>
2021-06-27 02:54:30 -0400

    feat:calendar grid items now have id day

--- src/render.js
+++ src/render.js
@@ -45,6 +45,7 @@ function makeRows(rows, cols) {
   calendar_grid.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
+    cell.id = "day"
     calendar_grid.appendChild(cell).className = "column";
   }
 }
