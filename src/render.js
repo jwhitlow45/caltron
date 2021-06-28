ae4711a1d1a011a979adc9728299d31b4d6cc7b5
jdubbleu <jwhitlow45@gmail.com>
2021-06-28 02:58:47 -0400

    feat:add highlighting of current day

--- src/render.js
+++ src/render.js
@@ -29,6 +29,7 @@ const months = [
   "December",
 ];
 
+const today = new Date(); //today
 var cur_month = new Date(); //month of current calendar view
 cur_month.setDate(1); //set to first of month to avoid conflict with certain months
 //end global values
@@ -98,17 +99,25 @@ function makeWeekdayHeader() {
 // draw month view grid
 function makeCalendarGrid() {
   const calendar_grid = document.getElementById("calendar-grid");
-  const padding_days = getPaddingDays(cur_month);
-  const days_in_month = daysInMonth(cur_month);
+  const padding_days = getPaddingDays(cur_month); //number of days to pad at start of first week
+  const days_in_month = daysInMonth(cur_month); //number of days in current month
   calendar_grid.style.setProperty("--grid-rows", rows);
   calendar_grid.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    if (c < padding_days || c > days_in_month + padding_days - 1) {
-      cell.id = "padding";
+    if (c < padding_days) {
+      cell.id = "padding"; //set cell as not in cur month
+    } else if (c > days_in_month + padding_days - 1) {
+      cell.id = "padding"; //set cell as not in cur month
     } else {
-      cell.id = "day";
-      cell.innerText = c - padding_days + 1;
+      num_date = c - padding_days + 1;
+      cell.id = "day"; //set cell as in cur month
+      //set inner text to match dates
+      cell.innerText = num_date;
+      //highlight todays date
+      if (num_date == today.getDate()) {
+        cell.id = "today";
+      }
     }
     calendar_grid.appendChild(cell).className = "column";
   }
