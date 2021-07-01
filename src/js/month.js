b49556ba62589b4ee476e2282efee11e13f9af41
jdubbleu <jwhitlow45@gmail.com>
2021-07-01 16:54:23 -0400

    refactor:comments

--- src/js/month.js
+++ src/js/month.js
@@ -13,25 +13,34 @@ function createMonthDayList() {
   const before_padding_days = getBeforePaddingDays(cur_month);
   for (c = 0; c < rows * cols; c++) {
     if (c < padding_days) {
+      // if current iteration is before current month
+      // create new date corresponding to current iteration
       temp_date = new Date(
         prev_month.getFullYear(),
         prev_month.getMonth(),
         days_in_prev_month + c - before_padding_days + 1
       );
+      // push to day list
       day_list.push(new Day(temp_date, true));
     } else if (c > days_in_cur_month + before_padding_days - 1) {
+      //if current iteration is after current month
+      // create new date corresponding to current iteration
       temp_date = new Date(
         next_month.getFullYear(),
         next_month.getMonth(),
         c - days_in_cur_month - before_padding_days + 1
       );
+      // push to day list
       day_list.push(new Day(temp_date, true));
     } else {
+      //if current iteration is in current month
+      // create new date corresponding to current iteration
       temp_date = new Date(
         cur_month.getFullYear(),
         cur_month.getMonth(),
         c - before_padding_days + 1
       );
+      // push to day list
       day_list.push(new Day(temp_date, false));
     }
   }
